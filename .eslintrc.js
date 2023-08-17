79f1fa061ad14eb905d725f9bc79d97e161405d0
Frank Calpito <fccalpitojr@gmail.com>
2023-08-17 11:16:41 -0600

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'react-hooks'
+  ],
+  settings: {
+    react: {
+      'version': '18.2.0',
+    },
+  },
+  rules: {
+    'react/no-unknown-property': [2, { ignore: ['jsx'] }]
+  },
+};
