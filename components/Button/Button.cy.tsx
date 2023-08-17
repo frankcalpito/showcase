94b3d29c106bfe8118eb11e42bece796c379d3e0
Frank Calpito <fccalpitojr@gmail.com>
2023-08-17 11:46:50 -0600

    Add cypress

---
+++ components/Button/Button.cy.tsx
@@ -0,0 +1,11 @@
+import { Button } from './Button'
+ 
+describe('<Button />', () => {
+  it('should render and display expected content', () => {
+    // Mount the Button component
+    cy.mount(<Button label="Test"/>);
+ 
+    // The button should contains Test
+    cy.get('button').contains('Test')
+  })
+})
