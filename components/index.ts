d100bae162a4d997310118e78e3512b7ef74cb4d
Frank Calpito <fccalpitojr@gmail.com>
2023-08-29 20:50:47 -0600

    add index

---
+++ components/index.ts
@@ -0,0 +1,15 @@
+
+
+import { Button } from "./Button/Button"
+import { Card } from "./Card/Card"
+import { FileUpload } from "./FileUpload/FileUpload"
+import { Input } from "./Input/Input"
+import { Loader } from "./Loader/Loader"
+
+export { 
+  Button,
+  Card,
+  FileUpload,
+  Input,
+  Loader
+}
